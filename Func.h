e616acc1642255f8c328ce008d91ac3a7ca4811f
for9int <wtmsb3770@gmail.com>
2026-05-11 00:29:22 +0800

    [feat] Func.h，加入图归一化函数

--- Func.h
+++ Func.h
@@ -3,6 +3,27 @@
 #include <random>
 #include <cmath>
 template<typename T>
+Matrix<T> normalizeAdjacency(const Matrix<T>& A){
+    int n = A.rows;
+    Matrix<T> A_hat = A;
+    for(int i = 0;i < n; ++i)
+        A_hat.set(i, i, 1);
+    // 计算度矩阵 D
+    std::vector<T> deg(n, 0.0f);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            deg[i] += A_hat.get(i, j);
+
+    // 构造 D^(-1/2)
+    Matrix<T> D_inv_sqrt(n, n);
+    for (int i = 0; i < n; ++i)
+        D_inv_sqrt.set(i, i, 1.0f / std::sqrt(deg[i] + 1e-8f)); // 避免除零
+
+    // 计算 Ĥ = D^(-1/2) * A_hat * D^(-1/2)
+    return D_inv_sqrt * A_hat * D_inv_sqrt;
+}
+
+
 
 template<typename T>
 void read_Graph(Matrix<T>& A){
