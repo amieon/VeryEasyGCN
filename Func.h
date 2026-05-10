e975d14745a485b2b257bc5b8edeabcc7e072749
for9int <wtmsb3770@gmail.com>
2026-05-11 00:29:54 +0800

    [feat] 向Func.h里加入Relu相关函数

--- Func.h
+++ Func.h
@@ -23,6 +23,54 @@ Matrix<T> normalizeAdjacency(const Matrix<T>& A){
     return D_inv_sqrt * A_hat * D_inv_sqrt;
 }
 
+template<typename T>
+Matrix<T> reluDerivative(const Matrix<T>& m){
+    Matrix<T> out(m.rows, m.cols);
+    for (int i = 0; i < m.rows; ++i)
+        for (int j = 0; j < m.cols; ++j)
+            out.set(i,j,(m.get(i,j) > 0) ? 1.0f : 0.0f);
+    return out;
+}
+
+template<typename T>
+Matrix<T> relu(const Matrix<T>& m){
+    Matrix<T> out(m.rows, m.cols);
+    for (int i = 0; i < m.rows; ++i)
+        for (int j = 0; j < m.cols; ++j)
+            out.set(i,j,(m.get(i,j) > 0) ? m.get(i,j) : 0.0f);
+    return out;
+}
+
+template<typename T>
+Matrix<T> MSELoss(const Matrix<T>& pred, const Matrix<T>& label) {
+    T loss = static_cast<T>(0);
+    int n = pred.rows;
+    int c = pred.cols;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < c; ++j) {
+            T diff = pred.get(i, j) - label.get(i, j);
+            loss += diff * diff;
+        }
+    }
+
+    Matrix<T> result(1, 1);
+    result.set(0, 0, loss / static_cast<T>(n * c));
+    return result;
+}
+
+template<typename T>
+Matrix<T> LossGrad(const Matrix<T>& pred, const Matrix<T>& label) {
+    int n = pred.rows;
+    int c = pred.cols;
+    Matrix<T> grad(n, c);
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < c; ++j)
+            grad.set(i, j, (pred.get(i,j) - label.get(i,j)) / n);
+
+    return grad;
+}
 
 
 template<typename T>
